ff5826b56b2f1f2eff82c50849475a1ceae6d7e2
Gabriel Cesario <gcesario203@gmail.com>
2023-02-11 12:58:05 -0300

    refact: Modularização

--- src/domain/repository/product.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import Product from "../entity/product";
-import RepositoryInterface from "./repository.interface";
-
-export default interface IProductRepository extends RepositoryInterface<Product> {
-    
-}
